e728c994b500908a733b42d815d1d79ad2adcbd4
FVIA <130783224+fviatool@users.noreply.github.com>
2024-01-20 15:26:42 +0700

    Update install.sh
    Signed-off-by: FVIA <130783224+fviatool@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -741,30 +741,6 @@ Install_Main(){
 }
 
 echo "
-
-red "Aapanel Pro "
-sed -i 's|"endtime": -1|"endtime": 999999999999|g' /www/server/panel/data/plugin.json
-sed -i 's|"pro": -1|"pro": 0|g' /www/server/panel/data/plugin.json
-chattr +i /www/server/panel/data/plugin.json
-chattr -i /www/server/panel/data/repair.json
-rm /www/server/panel/data/repair.json
-cd /www/server/panel/data
-wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/repair.json
-chattr +i /www/server/panel/data/repair.json
-rm /www/server/panel/data/plugin.json
-cd /www/server/panel/data
-wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/plugin.json
-chattr +i /www/server/panel/data/plugin.json
-rm /www/server/panel/BTPanel/static/js/soft.js
-cd /www/server/panel/BTPanel/static/js
-wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/soft.js
-chattr +i /www/server/panel/data/soft.js
-rm /www/server/panel/BTPanel/static/js/index.js
-cd /www/server/panel/BTPanel/static/js
-wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/index.js
-chattr +i /www/server/panel/data/index.js
-red "Done"
-
 +----------------------------------------------------------------------
 | aaPanel 6.0 FOR CentOS/Ubuntu/Debian
 +----------------------------------------------------------------------
@@ -807,4 +783,25 @@ endTime=`date +%s`
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
 rm -f install_6.0_en.sh
 
-
+ "Aapanel Pro "
+sed -i 's|"endtime": -1|"endtime": 999999999999|g' /www/server/panel/data/plugin.json
+sed -i 's|"pro": -1|"pro": 0|g' /www/server/panel/data/plugin.json
+chattr +i /www/server/panel/data/plugin.json
+chattr -i /www/server/panel/data/repair.json
+rm /www/server/panel/data/repair.json
+cd /www/server/panel/data
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/repair.json
+chattr +i /www/server/panel/data/repair.json
+rm /www/server/panel/data/plugin.json
+cd /www/server/panel/data
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/plugin.json
+chattr +i /www/server/panel/data/plugin.json
+rm /www/server/panel/BTPanel/static/js/soft.js
+cd /www/server/panel/BTPanel/static/js
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/soft.js
+chattr +i /www/server/panel/data/soft.js
+rm /www/server/panel/BTPanel/static/js/index.js
+cd /www/server/panel/BTPanel/static/js
+wget https://raw.githubusercontent.com/Github-Aiko/aapanel-pro/master/resource/index.js
+chattr +i /www/server/panel/data/index.js
+red "Done"
